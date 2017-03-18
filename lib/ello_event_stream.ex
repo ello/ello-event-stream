6961ba4b2aa0ad8af00277b5d7d4248f13d0bc29
Alan Peabody <gapeabody@gmail.com>
2017-03-18 10:46:48 -0400

    Initial commit
    Generated with:
    `mix new ello_event_stream --module=Ello.EventStream --sup`

---
+++ lib/ello_event_stream.ex
@@ -0,0 +1,18 @@
+defmodule Ello.EventStream do
+  @moduledoc """
+  Documentation for Ello.EventStream.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ello.EventStream.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ello_event_stream_test.exs
@@ -0,0 +1,8 @@
+defmodule Ello.EventStreamTest do
+  use ExUnit.Case
+  doctest Ello.EventStream
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
